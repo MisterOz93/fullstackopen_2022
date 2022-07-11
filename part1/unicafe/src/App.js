39f91996921f4c5be0953d2f99529a0626245129
Michael Mroz <mroz15@hotmail.com>
2022-07-11 14:28:36 -0400

    Complete exercise 1.7 - add total, average, and percent positive statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,55 @@
 import { useState } from 'react'
 
-const Button = ({onClick, name}) => {
+const Button = ( {onClick, name} ) => {
   return (
     <button onClick={onClick}>{name}</button>
   )
 }
 
+const Average = ( {scores, total} ) => {
+  const avg = !total ? 0 
+  :
+  scores.reduce((sum, num) => sum + num) / total
+  
+  return(
+    <p>Average: {avg} </p>
+  )
+}
+
+const PercentPositive = ( {scores, total} ) => {
+  const percentPositive = ! total ? 'n/a'
+  :
+  `${scores.filter(score => score === 1).length / 
+    total * 100}%`
+  return(
+    <p>Positive Reviews: {percentPositive}</p>
+  )
+
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [scores, setScores] = useState([])
+  //^good = +1 neut = 0 bad = -1 for scores
+
   const clickHandler = (review) => {
     if (review === 'g'){
       setGood(good + 1)
-      return
+      setScores(scores.concat(1))
     }
-    review === 'n' ? setNeutral(neutral + 1) 
-    : setBad(bad + 1)
+    else if (review === 'n'){
+      setNeutral(neutral + 1)
+      setScores(scores.concat(0))
+      
+    } 
+    else {
+      setBad(bad + 1) 
+      setScores(scores.concat(-1))
+    }
+    setTotal(total + 1)
   }
   return(
     <div>
@@ -29,6 +61,9 @@ const App = () => {
      <p>Good: {good}</p>
      <p>Neutral: {neutral}</p>
      <p>Bad: {bad}</p>
+     <p>Total: {total} </p>
+     <Average scores={scores} total={total} />
+     <PercentPositive scores={scores} total={total} />
     </div>
   )
 }
