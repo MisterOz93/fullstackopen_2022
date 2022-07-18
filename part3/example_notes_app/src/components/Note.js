59f89ce0fa4a9c356c32ee9c347a3397e0a2f131
Michael Mroz <mroz15@hotmail.com>
2022-07-18 14:32:43 -0400

    move and rename example notes app into part3 dir & create dir for part3 backend example app

---
+++ part3/example_notes_app/src/components/Note.js
@@ -0,0 +1,13 @@
+const Note = ( {note, toggleImportance} ) => {
+  const buttonLabel = note.important ?
+    "make unimportant" : "make important" 
+  return(
+    <li className='note'>
+      {note.content} {' '}
+      {<button onClick={toggleImportance}> { buttonLabel } </button>}
+    </li>
+  )
+
+}
+
+export default Note
