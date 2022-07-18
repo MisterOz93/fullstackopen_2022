e88a3c40c2bd739d47ece64e4a337b9a602934ec
Michael Mroz <mroz15@hotmail.com>
2022-07-18 19:27:18 -0400

    complete examples up to end of part 3a

---
+++ part3/part3_node_example/index.js
@@ -0,0 +1,77 @@
+const { response } = require('express')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(n => n.id === id)
+  note ? res.json(note) : res.status(404).end()
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
+const generateId = () => {
+  //method for generating id not recommended, will change soon
+  return notes.length > 0 
+    ? Math.max(...notes.map(n => n.id)) + 1
+    : 0
+  
+}
+
+app.post('/api/notes', (req, res) => {
+
+  if (!req.body.content){
+    return res.status(400).json({
+      error: 'content missing'}
+      )
+  }
+
+  const note = {
+    content: req.body.content,
+    important: req.body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+  notes.concat(note)
+  res.json(note)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
