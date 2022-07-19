9d8544ac7449107a93e174adcce3cb3584af5165
Michael Mroz <mroz15@hotmail.com>
2022-07-19 17:00:00 -0400

    complete example exercise up to part 3.2

--- part3/part3_node_example/index.js
+++ part3/part3_node_example/index.js
@@ -26,6 +26,16 @@ let notes = [
   }
 ]
 
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:', req.path)
+  console.log('Body:', req.body)
+  console.log('----')
+  next()
+}
+
+app.use(requestLogger)
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
 })
@@ -71,6 +81,12 @@ app.post('/api/notes', (req, res) => {
   notes.concat(note)
   res.json(note)
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
 
 
 const PORT = 3001
