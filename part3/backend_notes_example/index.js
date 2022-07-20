fd4612867e0e3ea1e6080dfa1490256416b60bb2
Michael Mroz <mroz15@hotmail.com>
2022-07-20 16:12:34 -0400

    create backend repo, add procfile, create frontend build and copy it to backend dir

--- part3/backend_notes_example/index.js
+++ part3/backend_notes_example/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
   {
@@ -91,5 +92,5 @@ const unknownEndpoint = (req, res) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
