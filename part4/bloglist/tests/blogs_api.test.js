338dd7e7d8b377b2e2baa913acad0cadd315bea2
Michael Mroz <mroz15@hotmail.com>
2022-08-01 15:58:21 -0400

    complete 4.8 - add test for GET route, refactor GET route to use async/await

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,5 +1,20 @@
 const hardCodedBlogs = require('../utils/list_helper').blogs
 const superTest = require('supertest')
+const Blog = require('../models/blog')
 const app = require('../app')
 const api = superTest(app)
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of hardCodedBlogs){
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('GET request returns all blogs as JSON', async () => {
+  const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body).toHaveLength(hardCodedBlogs.length)
+})
