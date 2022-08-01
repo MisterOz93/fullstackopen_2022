60eee5d5855579f821a463b23a335c84a582f0d1
Michael Mroz <mroz15@hotmail.com>
2022-08-01 14:12:07 -0400

    add express async errors library, remove try/catch blocks from async requests

--- part4/notes_backend/controllers/notes.js
+++ part4/notes_backend/controllers/notes.js
@@ -1,4 +1,4 @@
-//cont. from Eliminating the try-catch
+require('express-async-errors')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
@@ -8,55 +8,40 @@ notesRouter.get('/', async (req, res) => {
   res.json(notes)
 })
 
-notesRouter.get('/:id', async (req, res, next) => {
-  try {
-    const note = await Note.findById(req.params.id)
-    return note ? res.json(note) : res.status(404).end()
-  } catch(error){
-    next(error) }
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  return note ? res.json(note) : res.status(404).end()
 })
 
-notesRouter.delete('/:id', async (req, res, next) => {
-  try {
-    await Note.findByIdAndRemove(req.params.id)
-    res.status(204).end()
-  } catch(error) {
-    next(error)
-  }
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndRemove(req.params.id)
+  res.status(204).end()
 })
 
-notesRouter.post('/', async (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
 
   const note = new Note({
     content: req.body.content,
     important: req.body.important || false,
     date: new Date(),
   })
-  try {
-    const savedNote = await note.save()
-    res.status(201).json(savedNote)
-  }
-  catch(error) {
-    next(error)
-  }
+  const savedNote = await note.save()
+  res.status(201).json(savedNote)
 })
 
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', async (req, res) => {
 
   const body = req.body
   const note = {
     content: body.content,
     important: body.important
   }
-  Note.findByIdAndUpdate(
+  const updatedNote = await Note.findByIdAndUpdate(
     req.params.id,
     note,
     { new: true, runValidators: true, context: 'query' }
   )
-    .then(updatedNote => {
-      res.json(updatedNote)
-    }).catch(error => next(error))
-
+  res.json(updatedNote)
 })
 
 module.exports = notesRouter
