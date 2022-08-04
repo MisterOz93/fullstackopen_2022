3c5f28e1f69226627a714af9ba0ffee2d5bf7d22
Michael Mroz <mroz15@hotmail.com>
2022-08-03 20:20:27 -0400

    add bcrypt package, create schema for User and create post route for user

---
+++ part4/notes_backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.schema({
+  username: String,
+  name: String,
+  passWordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ],
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passWordHash
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
