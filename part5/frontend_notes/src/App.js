7805ed65c5feb129e38ded5b6790a5ab2872bb1f
Michael Mroz <mroz15@hotmail.com>
2022-08-09 21:09:34 -0400

    begin adding login to notes frontend example app

--- part5/frontend_notes/src/App.js
+++ part5/frontend_notes/src/App.js
@@ -1,22 +1,27 @@
+//cont. with taking notes in frontend login txt file
 import { useState, useEffect } from 'react'
 //import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
+import loginService from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    console.log('effect')
+    //console.log('effect')
     noteService.getAll().then(notes => setNotes(notes))
   }, [])
 
-  console.log('render', notes.length, 'notes');
+  //console.log('render', notes.length, 'notes');
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
@@ -56,10 +61,60 @@ const App = () => {
       })
   }
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    try {
+      const user = loginService.login({ username, password })
+      setUser(user)
+      console.log('logging in with', user);
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Invalid Username or Password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        Username: 
+          <input 
+            type='text'
+            value={username}
+            name="Username"
+            onChange={({target}) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        Password:
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({target}) => setPassword(target.value)}
+         />                
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit'>Save</button>
+      </form>
+  )
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {user == null && loginForm()}
+      {user !== null && noteForm()}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important notes' : 'all'}
@@ -70,10 +125,6 @@ const App = () => {
           <Note key={note.id} note={note} toggleImportance={() => toggleImportance(note.id)}/>
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type='submit'>Save</button>
-      </form>
       <Footer />
     </div>
   )
