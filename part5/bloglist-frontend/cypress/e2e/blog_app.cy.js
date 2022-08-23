be8d70ca42bcea4cbf1952d79f00e79de6963a7f
Michael Mroz <mroz15@hotmail.com>
2022-08-22 20:34:58 -0400

    complete 5.22 - add cy test ensuring blogs are listed by likes

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -37,6 +37,9 @@ describe('Blog app', function() {
   describe.only('When logged in', function() {
     beforeEach(function() {
       cy.login({ username: 'Tester', password: 'password' })
+      cy.createBlog({ title: 'blog a', author: 'Tester', url: '1234' })
+      cy.createBlog({ title: 'blog b', author: 'Tester', url: '1234' })
+      cy.createBlog({ title: 'blog c', author: 'Tester', url: '1234' })
     })
 
     it('A blog can be created', function() {
@@ -51,24 +54,35 @@ describe('Blog app', function() {
     })
 
     it('A blog can be liked by the user', function(){
-      cy.createBlog({ title: 'blog a', author: 'Tester', url: '1234' })
-      cy.createBlog({ title: 'blog b', author: 'Tester', url: '1234' })
-      cy.createBlog({ title: 'blog c', author: 'Tester', url: '1234' })
       cy.get('.blog').contains('blog b').contains('View').click()
         .get('#like_button').click()
       cy.get('.blog').contains('blog b').parent().contains('Likes: 1')
     })
 
-    it.only('A blog can be deleted by its creator', function(){
-      cy.createBlog({ title: 'blog a', author: 'Tester', url: '1234' })
+    it('A blog can be deleted by its creator', function(){
       cy.createBlog({ title: 'delete me', author: 'Tester', url: '1234' })
-      cy.createBlog({ title: 'blog c', author: 'Tester', url: '1234' })
-      cy.get('.blog').should('have.length', '3')
+      cy.get('.blog').should('have.length', 4)
       cy.get('.blog').contains('delete me').parent().contains('View').click()
       cy.contains('Delete').click()
-      cy.get('.blog').should('have.length', 2)
+      cy.get('.blog').should('have.length', 3)
+    })
 
-
+    it.only('Blogs are displayed starting from most liked', function(){
+      cy.createBlog({ title: 'like me the most', author: 'Tester', url: '1234' })
+      cy.get('.blog').should('have.length', 4)
+      cy.get('.blog').contains('like me the most').contains('View').click()
+        .get('#like_button').click().click().click()
+      cy.get('.blog').contains('blog b').parent().as('blogb')
+      cy.get('@blogb').contains('View').click()
+      cy.get('@blogb').contains('Like').click().click()
+      cy.createBlog({ title: 'like me third most!', author: 'Tester', url: 'w/e' })
+      cy.get('.blog').should('have.length', 5)
+      cy.get('.blog').contains('like me third most!').contains('View').click()
+        .parent().parent().contains('Like').click()
+      cy.wait(1000)
+      cy.get('.blog').eq(0).should('contain', 'like me the most')
+      cy.get('.blog').eq(1).should('contain', 'blog b')
+      cy.get('.blog').eq(2).should('contain', 'like me third most!')
 
     })
 
