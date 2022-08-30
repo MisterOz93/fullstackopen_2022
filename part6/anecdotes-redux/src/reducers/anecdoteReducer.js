8d175c8cf98c778e3d58385619131f812d4b9b7d
Michael Mroz <mroz15@hotmail.com>
2022-08-30 18:06:25 -0400

    complete 6.13 - add json server and axios, app now sets anecdotes from backend

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,11 +10,9 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     addAnecdote(state, action) {
       state.push(asObject(action.payload))
@@ -38,13 +27,18 @@ const anecdoteSlice = createSlice({
       return state.map(a => a.id !== id ? a : changedAnecdote)
         .sort((a, b) => b.votes - a.votes)
     },
+    
     sortAnecdotes(state) {
       return state.sort((a, b) => a.votes - b.votes)
+    },
+
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 
 })
 
-export const {addAnecdote, addVote, sortAnecdotes} = anecdoteSlice.actions
+export const {addAnecdote, addVote, setAnecdotes} = anecdoteSlice.actions
 
 export default anecdoteSlice.reducer
