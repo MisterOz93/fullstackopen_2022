beed00f51b89b20e7dd858e7367814029c4c4866
Michael Mroz <mroz15@hotmail.com>
2022-08-31 18:32:41 -0400

    complete note example up to exercise 6.15. add redux-thunk to anecdotes app

--- part6/redux_notes/src/App.js
+++ part6/redux_notes/src/App.js
@@ -1,18 +1,17 @@
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
-import noteService from './services/notes'
 import { useEffect } from 'react'
-import { setNotes } from './reducers/noteReducer'
+import { initializeNotes } from './reducers/noteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
+
   useEffect(() => {
-    noteService.getAll().then(notes => {
-      dispatch(setNotes(notes))
-    })
+    dispatch(initializeNotes())
   }, [dispatch])
+
   return(
     <div>
       <NewNote />
