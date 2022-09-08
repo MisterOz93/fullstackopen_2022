f8652a2547eb4183ee63f4036fcfd428983254dd
Michael Mroz <mroz15@hotmail.com>
2022-09-08 17:38:59 -0400

    complete 7.4 - refactor forms to use custom useField hook

--- part7/anecdotes-router/src/App.js
+++ part7/anecdotes-router/src/App.js
@@ -5,6 +5,8 @@ import {
   useParams, useNavigate
 } from 'react-router-dom'
 
+import { useField } from './hooks'
+
 const AnecdoteList = ({ anecdotes }) => {
 
   return(
@@ -64,21 +66,26 @@ const Footer = () => (
 
 const CreateNew = (props) => {
 
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+ 
   const nav = useNavigate()
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     nav('/')
 
+  }
+  const margin = {
+    'marginLeft': 5
   }
 
   return (
@@ -87,15 +94,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input style={margin} {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input style={margin} {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input style={margin} {...info} />
         </div>
         <button>create</button>
       </form>
@@ -127,6 +134,7 @@ const App = () => {
   let clearNotification
 
   const addNew = (anecdote) => {
+    console.log('anecdote is', anecdote);
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
     if (typeof clearNotification === 'number'){
