618759354a1e22d3d6afda52fb2375d3c0613178
Michael Mroz <mroz15@hotmail.com>
2022-09-10 20:43:26 -0400

    add react bootstrap to example router app and create table for notes

--- part7/router-example-app/src/index.js
+++ part7/router-example-app/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM  from 'react-dom/client'
 
+import { Table } from 'react-bootstrap'
+
 import { useState } from 'react'
 
 import {
@@ -35,13 +37,20 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+        </tr>
       )}
-    </ul>
+    </tbody>
+    </Table>
   </div>
 )
 
