257f07f055e40b267db1899f5cb1bc07b4705044
Michael Mroz <mroz15@hotmail.com>
2022-09-16 21:03:04 -0400

    change webpack config to return function that accepts env and argv

--- part7/react-app-from-scratch/src/App.js
+++ part7/react-app-from-scratch/src/App.js
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
-import './index.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, SetCounter] = useState(0)
   const [values, setValues] = useState([])
+  const url = 'https://obscure-harbor-49797.herokuapp.com/api/notes'
+  const notes = useNotes(BACKEND_URL)
 
-  const hanldeClick = () => {
+  const handleClick = () => {
     SetCounter(counter + 1)
     setValues(values.concat(counter))
   }
   return (
     <div className='container'>
       hello webpack {counter} clicks.
-      <button onClick={hanldeClick}>
+      <button onClick={handleClick}>
         Press
       </button>
+      <div>{notes.length} notes on server {BACKEND_URL}</div>
     </div>
   )
 }
