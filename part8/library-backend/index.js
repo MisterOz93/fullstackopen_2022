c88c16ab3aa795ee5efca53d35dbdd26a53cc373
Michael Mroz <mroz15@hotmail.com>
2022-10-06 20:44:47 -0400

    complete 8.7 - add editAuthor mutation

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -123,6 +123,8 @@ const typeDefs = gql`
       published: Int
       genres: [String!]!
     ): Book
+
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -172,6 +174,19 @@ const resolvers = {
       }
       books = books.concat(newBook)
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = {
+        ...author,
+        born: args.setBornTo,
+      }
+
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
     },
   },
 }
