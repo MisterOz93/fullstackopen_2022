bd385e49069aee44b2a234669096feb901d2c96f
Michael Mroz <mroz15@hotmail.com>
2022-10-18 21:03:57 -0400

    add login mutation, add conditional render of login form, and add loginForm component

--- part8/frontend-phonebook_graphql/src/App.js
+++ part8/frontend-phonebook_graphql/src/App.js
@@ -1,3 +1,5 @@
+//continue from loginform code overview
+
 import { useQuery } from '@apollo/client'
 import { useState } from 'react'
 import Persons from './components/Persons'
@@ -5,9 +7,11 @@ import PersonForm from './components/PersonForm'
 import Notification from './components/Notification'
 import { ALL_PERSONS } from './queries'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
 
   if (result.loading) {
@@ -19,6 +23,16 @@ const App = () => {
     setTimeout(() => {
       setErrorMessage(null)
     }, 6000)
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notification errorMessage={errorMessage} />
+        <h2>Log In</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    )
   }
 
   return (
