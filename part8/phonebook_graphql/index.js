581b0d962370f7b897ea84344bffca75c88b6295
Michael Mroz <mroz15@hotmail.com>
2022-10-28 20:39:20 -0400

    add apollo server express and re organize phonebook dir for fragments and subscriptions part

--- part8/phonebook_graphql/index.js
+++ part8/phonebook_graphql/index.js
@@ -1,10 +1,12 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const Person = require('./models/persons')
 const User = require('./models/user')
 const { MONGODB_URI } = require('./utils/config')
 const JWT_SECRET = 'SUPER_SECRET_KEY'
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
 
 console.log('connecting to', MONGODB_URI)
 mongoose
@@ -15,156 +17,6 @@ mongoose
   .catch((error) => {
     console.log('Error connecting to MongoDB:', error.message)
   })
-
-const typeDefs = gql`
-  type Address {
-    street: String!
-    city: String!
-  }
-
-  type Person {
-    name: String!
-    phone: String
-    address: Address!
-    id: ID!
-  }
-
-  type User {
-    username: String!
-    friends: [Person!]!
-    id: ID!
-  }
-
-  type Token {
-    value: String!
-  }
-
-  enum YesNo {
-    YES
-    NO
-  }
-
-  type Query {
-    personCount: Int!
-    allPersons(phone: YesNo): [Person!]!
-    findPerson(name: String!): Person
-    me: User
-  }
-
-  type Mutation {
-    addPerson(
-      name: String!
-      phone: String
-      street: String!
-      city: String!
-    ): Person
-
-    editNumber(name: String!, phone: String!): Person
-
-    createUser(username: String!): User
-
-    login(username: String!, password: String!): Token
-
-    addAsFriend(name: String!): User
-  }
-`
-
-const resolvers = {
-  Query: {
-    personCount: async () => Person.collection.countDocuments(),
-    allPersons: async (root, args) => {
-      if (!args.phone) {
-        return Person.find({})
-      }
-      return Person.find({ phone: { $exists: args.phone === 'YES' } })
-    },
-    findPerson: async (root, args) => Person.findOne({ name: args.name }),
-    me: (root, args, context) => context.currentUser,
-  },
-  Person: {
-    address: (root) => {
-      return {
-        street: root.street,
-        city: root.city,
-      }
-    },
-  },
-  Mutation: {
-    addPerson: async (root, args, context) => {
-      const person = new Person({ ...args })
-      const currentUser = context.currentUser
-      if (!currentUser) {
-        throw new AuthenticationError('Authentication Failed')
-      }
-      try {
-        await person.save()
-        currentUser.friends = currentUser.friends.concat(person)
-        await currentUser.save()
-      } catch (error) {
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        })
-      }
-      return person
-    },
-
-    editNumber: async (root, args) => {
-      const person = await Person.findOne({ name: args.name })
-      person.phone = args.phone
-      try {
-        await person.save()
-      } catch (error) {
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        })
-      }
-      return person
-    },
-
-    createUser: async (root, args) => {
-      const user = User({ username: args.username })
-      return user.save().catch((error) => {
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        })
-      })
-    },
-
-    login: async (root, args) => {
-      const user = await User.findOne({ username: args.username })
-
-      if (!user || args.password !== 'password') {
-        throw new UserInputError('Invalid Username or Password')
-      }
-
-      const userForToken = {
-        username: user.username,
-        id: user._id,
-      }
-
-      return { value: jwt.sign(userForToken, JWT_SECRET) }
-    },
-
-    addAsFriend: async (root, args, { currentUser }) => {
-      const isFriend = (person) => {
-        currentUser.friends
-          .map((f) => f._id.toString())
-          .includes(person._id.toString())
-      }
-      if (!currentUser) {
-        throw new AuthenticationError('User Authentication failed')
-      }
-
-      const person = Person.findOne({ name: args.name })
-
-      if (!isFriend(person)) {
-        currentUser.friends = currentUser.friends.concat(person)
-      }
-      await currentUser.save()
-      return currentUser
-    },
-  },
-}
 
 const server = new ApolloServer({
   typeDefs,
---
+++ part8/phonebook_graphql/resolvers.js
@@ -0,0 +1,105 @@
+const { UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
+const Person = require('./models/persons')
+const User = require('./models/user')
+
+const JWT_SECRET = 'SUPER_SECRET_KEY'
+
+const resolvers = {
+  Query: {
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
+      if (!args.phone) {
+        return Person.find({})
+      }
+      return Person.find({ phone: { $exists: args.phone === 'YES' } })
+    },
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => context.currentUser,
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      }
+    },
+  },
+  Mutation: {
+    addPerson: async (root, args, context) => {
+      const person = new Person({ ...args })
+      const currentUser = context.currentUser
+      if (!currentUser) {
+        throw new AuthenticationError('Authentication Failed')
+      }
+      try {
+        await person.save()
+        currentUser.friends = currentUser.friends.concat(person)
+        await currentUser.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+      return person
+    },
+
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      try {
+        await person.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+      return person.save()
+    },
+
+    createUser: async (root, args) => {
+      const user = User({ username: args.username })
+      return user.save().catch((error) => {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      })
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'password') {
+        throw new UserInputError('Invalid Username or Password')
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    },
+
+    addAsFriend: async (root, args, { currentUser }) => {
+      const isFriend = (person) => {
+        currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString())
+      }
+      if (!currentUser) {
+        throw new AuthenticationError('User Authentication failed')
+      }
+
+      const person = Person.findOne({ name: args.name })
+
+      if (!isFriend(person)) {
+        currentUser.friends = currentUser.friends.concat(person)
+      }
+      await currentUser.save()
+      return currentUser
+    },
+  },
+}
+
+module.exports = resolvers
