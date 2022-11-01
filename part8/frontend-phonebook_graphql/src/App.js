1d7725ceec4e6c7d48adf5564b0a79e9a9770c68
Michael Mroz <mroz15@hotmail.com>
2022-10-31 20:59:30 -0400

    complete phonebook app examples up to n+1 problem section

--- part8/frontend-phonebook_graphql/src/App.js
+++ part8/frontend-phonebook_graphql/src/App.js
@@ -1,19 +1,40 @@
-//continue from loginform code overview
-
-import { useApolloClient, useQuery } from '@apollo/client'
+import { useApolloClient, useQuery, useSubscription } from '@apollo/client'
 import { useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Notification from './components/Notification'
-import { ALL_PERSONS } from './queries'
+import { ALL_PERSONS, PERSON_ADDED } from './queries'
 import PhoneForm from './components/PhoneForm'
 import LoginForm from './components/LoginForm'
+
+export const updateCache = (cache, query, addedPerson) => {
+  const uniqueByName = (arr) => {
+    let seen = new Set()
+    return arr.filter((i) => {
+      return seen.has(i.name) ? false : seen.add(i.name)
+    })
+  }
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqueByName(allPersons.concat(addedPerson)),
+    }
+  })
+}
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
   const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const client = useApolloClient()
+
+  useSubscription(PERSON_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      const addedPerson = subscriptionData.data.personAdded
+      notify(`${addedPerson.name} added`)
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson)
+    },
+  })
 
   if (result.loading) {
     return <div>loading...</div>
