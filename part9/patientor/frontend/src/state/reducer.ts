ff541b7d8870de918efba6c802dab85fe1e26a46
Michael Mroz <mroz15@hotmail.com>
2022-11-24 21:02:01 -0500

    fix eslint warning in frontend, ready for exercise 9.16

--- part9/patientor/frontend/src/state/reducer.ts
+++ part9/patientor/frontend/src/state/reducer.ts
@@ -1,19 +1,19 @@
-import { State } from "./state";
-import { Patient } from "../types";
+import { State } from './state'
+import { Patient } from '../types'
 
 export type Action =
   | {
-      type: "SET_PATIENT_LIST";
-      payload: Patient[];
+      type: 'SET_PATIENT_LIST'
+      payload: Patient[]
     }
   | {
-      type: "ADD_PATIENT";
-      payload: Patient;
-    };
+      type: 'ADD_PATIENT'
+      payload: Patient
+    }
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_PATIENT_LIST":
+    case 'SET_PATIENT_LIST':
       return {
         ...state,
         patients: {
@@ -21,18 +21,18 @@ export const reducer = (state: State, action: Action): State => {
             (memo, patient) => ({ ...memo, [patient.id]: patient }),
             {}
           ),
-          ...state.patients
-        }
-      };
-    case "ADD_PATIENT":
+          ...state.patients,
+        },
+      }
+    case 'ADD_PATIENT':
       return {
         ...state,
         patients: {
           ...state.patients,
-          [action.payload.id]: action.payload
-        }
-      };
+          [action.payload.id]: action.payload,
+        },
+      }
     default:
-      return state;
+      return state
   }
-};
+}
