0f26eae602a4721b9433b8e820a59bf927486af3
Michael Mroz <mroz15@hotmail.com>
2022-12-06 18:31:27 -0400

    complete HealthCheck entry component, create function to return color for heart based on rating

--- part9/patientor/frontend/src/components/HealthCheckEntry.tsx
+++ part9/patientor/frontend/src/components/HealthCheckEntry.tsx
@@ -24,12 +24,27 @@ const HealthCheckEntryDetails = ({entry, diagnoses}: entryProps) => {
   };
   console.log(entry.healthCheckRating);
 
+  const iconColour = ():string => {
+    switch(entry.healthCheckRating) {
+      case(0):
+        return "green";
+      case(1):
+        return "yellow";
+      case(2):
+        return "orange";
+      case(3):
+        return "red";
+      default:
+        return "pink";
+    }
+  }
+
   return(
     <div id='container' style={entriesStyle}>
       <div style={innerMargin}>
         <p>{entry.date + ' '} {'  '}<CalendarMonthIcon/></p>
         <p><em>{entry.description}</em> </p>
-        <FavoriteIcon style={{fill: "green"}}/>
+        <FavoriteIcon style={{fill: iconColour()}}/>
         <ul>
           {entry.diagnosisCodes?.map(code => 
             <li key={code}>{code} {diagnoses[codeIndex(code)].name}</li>
